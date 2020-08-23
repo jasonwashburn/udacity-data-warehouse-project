2b4937c29eeb0a3ebcc9f2d4ce199083e65205e4
Jason Washburn <wburn@wburn.net>
2020-08-23 08:58:51 -0500

    Adds table create and drop statements

--- sql_queries.py
+++ sql_queries.py
@@ -7,13 +7,13 @@ config.read('dwh.cfg')
 
 # DROP TABLES
 
-staging_events_table_drop = ""
-staging_songs_table_drop = ""
-songplay_table_drop = ""
-user_table_drop = ""
-song_table_drop = ""
-artist_table_drop = ""
-time_table_drop = ""
+staging_events_table_drop = "DROP TABLE IF EXISTS staging_events"
+staging_songs_table_drop = "DROP TABLE IF EXISTS statging_songs"
+songplay_table_drop = "DROP TABLE IF EXISTS songplays"
+user_table_drop = "DROP TABLE IF EXISTS users"
+song_table_drop = "DROP TABLE IF EXISTS songs"
+artist_table_drop = "DROP TABLE IF EXISTS artists"
+time_table_drop = "DROP TABLE IF EXISTS time"
 
 # CREATE TABLES
 
@@ -23,19 +23,60 @@ staging_events_table_create= ("""
 staging_songs_table_create = ("""
 """)
 
-songplay_table_create = ("""
+songplay_table_create = ("""CREATE TABLE IF NOT EXISTS songplays 
+                            (
+                            songplay_id bigint IDENTITY PRIMARY KEY,
+                            start_time bigint NOT NULL, 
+                            user_id int NOT NULL, 
+                            level text, 
+                            song_id text,
+                            artist_id text, 
+                            session_id int, 
+                            location text,
+                            user_agent text
+                            )
 """)
 
-user_table_create = ("""
+user_table_create = ("""CREATE TABLE IF NOT EXISTS users 
+                        (
+                            user_id int PRIMARY KEY,
+                            first_name text, 
+                            last_name text, 
+                            gender char, 
+                            level text
+                        )
 """)
 
-song_table_create = ("""
+song_table_create = ("""CREATE TABLE IF NOT EXISTS songs 
+                        (
+                            song_id text PRIMARY KEY, 
+                            title text,
+                            artist_id text, 
+                            year int, 
+                            duration float
+                        )
 """)
 
-artist_table_create = ("""
+artist_table_create = ("""CREATE TABLE IF NOT EXISTS artists 
+                        (
+                            artist_id text PRIMARY KEY,
+                            name text, 
+                            location text, 
+                            latitude float, 
+                            longitude float
+                        )
 """)
 
-time_table_create = ("""
+time_table_create = ("""CREATE TABLE IF NOT EXISTS time 
+                        (
+                            start_time bigint PRIMARY KEY,
+                            hour int, 
+                            day int, 
+                            week int, 
+                            month int, 
+                            year int,
+                            weekday int
+                        )
 """)
 
 # STAGING TABLES
